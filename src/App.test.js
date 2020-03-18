190e29d7364be7368b1bb9006406e93a055e7a2c
wjdwndud0114 <wjdwndud0114@yahoo.com>
2020-03-18 04:28:40 -0700

    Fix test and add queries for employeeSkill

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,20 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
+import { Provider } from "react-redux";
+import configureStore from "./configureStore";
+import Amplify from "aws-amplify";
+import exports from "./aws-exports";
 
+// TODO: mock this or API.graphql
+Amplify.configure(exports);
 describe("App component test", () => {
   test("renders the appbar with menu button", () => {
-    const { getByRole } = render(<App />);
+    const { getByRole } = render(
+      <Provider store={configureStore()}>
+        <App />
+      </Provider>
+    );
     const linkElement = getByRole("button", { name: /Open drawer/i });
     expect(linkElement).toBeVisible();
   });
